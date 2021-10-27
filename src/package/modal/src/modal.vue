6d93889f403b702fa936a4ad3eaa9a35108ae457
hexiandong <hexiandong@linklogis.com>
2021-10-27 18:09:25 +0800

    feat: 优化modal组件拖拽功能代码

--- src/package/modal/src/modal.vue
+++ src/package/modal/src/modal.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import {ref, watch, computed, onMounted, onUpdated} from 'vue';
+import {ref, watch, computed, onMounted, onUpdated, nextTick} from 'vue';
 import {formatSize} from '@/utils/tools';
 import {getBrowserWidth} from '@/utils/dom';
 
@@ -125,16 +125,16 @@ export default {
       }]
     });
 
-    const width = ref(formatSize(props.size));
-    const margin = ref({margin: 'auto'});
-    const left = ref('auto');
-    const top = ref('auto');
+    const marginLeft = ref('auto');
+    const marginTop = ref('auto');
     const pageX = ref(0);
     const pageY = ref(0);
 
     const mainStyle = computed(() => {
-      const style = {width: formatSize(props.width), marginLeft: left.value, marginTop: top.value};
-      return style
+      return {
+        width: formatSize(props.width),
+        marginLeft: marginLeft.value, marginTop: marginTop.value
+      }
     });
     const footStyle = computed(() => {
       const placementMaps = {
@@ -164,6 +164,11 @@ export default {
       el.style.transition = 'opacity .3s ease,transform .3s ease,scale .3s ease'
       //done()
     };
+
+    let maxMarginLeft = 0;
+    let maxMarginTop = 0;
+    let innerX = 0;
+    let innerY = 0;
     // 鼠标按钮按下去事件
     const handleMouseDown = (event) => {
       console.log('handleMouseDown');
@@ -171,9 +176,13 @@ export default {
         return
       }
       canDrag.value = true;
-      pageX.value = event.clientX - lkuModal.value.offsetLeft;
-      pageY.value = event.clientY - lkuModal.value.offsetTop;
+      // 表示弹窗marginLeft/Top的最大值 = （浏览器可视宽/高-弹窗宽/高）
+      maxMarginLeft = window.innerWidth - lkuModal.value.offsetWidth;
+      maxMarginTop = window.innerHeight - lkuModal.value.offsetHeight;
 
+      // 表示获取鼠标点的一瞬间，到modal左边/顶部的距离
+      innerX = event.clientX - lkuModal.value.offsetLeft;
+      innerY = event.clientY - lkuModal.value.offsetTop;
     };
     // 鼠标按钮松开事件
     const handleMouseUp = () => {
@@ -184,60 +193,20 @@ export default {
       if (!props.draggable || !canDrag.value) {
         return
       }
-      const marginWidth = calcMoveWidth(pageX, pageY);
-      margin.value = marginWidth;
+      calcMoveWidth(pageX, pageY);
       emit('drag');
     }
     const calcMoveWidth = (event) => {
-      const {clientWidth, clientHeight} = getBrowserWidth();
-      console.log(lkuModal.value.offsetLeft, lkuModal.value.offsetTop, lkuModal.value.offset);
-
-
-      top.value = event.clientY - pageY.value + 'px'
-      console.log(left.value, top.value);
-
-      const positionLeft = window.innerWidth - lkuModal.value.offsetWidth;
-      const positionTop = window.innerHeight - lkuModal.value.offsetHeight;
-
-      //left.value = Math.max(event.clientX - pageX.value, positionLeft);
-
-      if (event.clientX - pageX.value >= 0 && event.clientX - pageX.value <= positionLeft) {
-        left.value = event.clientX - pageX.value + 'px'
+      // 防止弹窗水平方向 被拖拽出浏览器
+      if (event.clientX - innerX >= 0 && event.clientX - innerX <= maxMarginLeft) {
+        marginLeft.value = event.clientX - innerX + 'px'
+        // (event.clientX - innerX ),表示当前clientX - 鼠标开始点的时候原始clientX = 变化量  + 原始offsetWidth,
+        // 变化量为整数，表示X轴向右拖动，否则向左
       }
-
-      console.log(event.clientY - pageY.value);
-      if (event.clientY - pageY.value >= 0 && event.clientY - pageY.value <= positionTop) {
-        top.value = event.clientY - pageY.value + 'px'
+      // 防止弹窗垂直方向 被拖拽出浏览器
+      if (event.clientY - innerY >= 0 && event.clientY - pageY.value <= maxMarginTop) {
+        marginTop.value = event.clientY - innerY + 'px'
       }
-
-      // if (event.clientX - pageX.value >= positionLeft) {
-      //   left.value = positionLeft + 'px'
-      // }
-      // if (event.clientY - pageY.value <= 0) {
-      //   top.value = 0;
-      // }
-      let size = 0;
-      // switch (props.direction) {
-      //   case 'left':
-      //     size = event.pageX;
-      //     break;
-      //   case 'right':
-      //     // pageX和Y可以为负数, 如果为负数，说明左侧拉到底了，则最大宽度取clientWidth，否则取clientWidth - event.pageX
-      //     size = Math.min(clientWidth - event.pageX, clientWidth); // 0<=size<=clientHeight
-      //     break;
-      //   case 'top':
-      //     size = event.pageY;
-      //     break;
-      //   case 'bottom':
-      //     size = Math.min(clientHeight - event.pageY, clientHeight);
-      //     break;
-      //   default:
-      //     return
-      // }
-      // return {
-      //   marginLeft: (event.clientX - lkuModal.value.offsetLeft) + 'px',
-      //   marginTop: (event.clientY - lkuModal.value.offsetTop) + 'px'
-      // };
     };
     const handleCancel = (event) => {
       // 点击取消默认是关闭弹窗
