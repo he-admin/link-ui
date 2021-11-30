e60426fee4b8319b15af7995231d99b641f668c6
hexiandong <1170833484@qq.com>
2021-12-01 00:25:36 +0800

    build: 上传项目打包dist文件夹

---
+++ examples/docs/zh_CN/demo2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>{{ value1 }}</div>
+    <lku-switch v-model="value1" active-color="#ff0000" inactive-color="#984747"></lku-switch>
+    <lku-switch v-model="value1" size="large" loading></lku-switch>
+    <lku-switch v-model="value1" size="small"></lku-switch>
+    <lku-switch v-model="value1" size="small">
+      <template v-slot:open>
+        <span>开</span>
+      </template>
+      <template v-slot:close>
+        <span>关</span>
+      </template>
+    </lku-switch>
+    <lku-switch v-model="value1" size="large">
+      <template v-slot:open>
+        <span>开</span>
+      </template>
+      <template v-slot:close>
+        <span>关</span>
+      </template>
+    </lku-switch>
+  </div>
+
+</template>
+<script>
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    const value1 = ref(true);
+    return {value1}
+  }
+}
+</script>
